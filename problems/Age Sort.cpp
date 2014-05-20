2d0e9464517a3dc5a9506ec989b490c47a115676
Matheus Sampaio <matheus1401@gmail.com>
2014-05-19 23:34:51 -0300

    Add Age Sort and Parentheses Balance

---
+++ problems/Age Sort.cpp
@@ -0,0 +1,75 @@
+#include <cstdio>
+#include <iostream>
+#include <map>
+#include <algorithm>
+
+/*
+Problem:
+
+Link: http://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=2457
+
+Age Sort
+Input: Standard Input
+Output: Standard Output
+Time limit: 5.000 seconds
+
+You are given the ages (in years) of all people of a country with at least 1 year of age. You know that no individual in that country lives for 100 or more years. Now, you are given a very simple task of sorting all the ages in ascending order.
+
+Input
+There are multiple test cases in the input file. Each case starts with an integer n (0<n<=2000000), the total number of people. In the next line, there are n integers indicating the ages. Input is terminated with a case where n = 0. This case should not be processed.
+
+Output
+For each case, print a line with n space separated integers. These integers are the ages of that country sorted in ascending order.
+
+Warning: Input Data is pretty big (~  25 MB) so use faster IO.
+
+Sample Input
+5
+3 4 2 1 5
+5
+2 3 2 3 1
+0
+
+Output for Sample Input
+1 2 3 4 5
+1 2 2 3 3
+
+Note: The memory limit of this problem is 2 Megabyte Only.
+
+*/
+
+using namespace std;
+
+int num, i;
+map<int, int> ages;
+map<int, int>::iterator iter;
+
+int main() {
+
+    freopen("input.txt", "r", stdin);
+
+    int N;
+
+    while (true) {
+        scanf("%d", &N);
+
+        if (!N)
+            break;
+
+        for (i = 0; i < N; i++) {
+            scanf("%d", &num);
+            ages[num]++;
+        }
+
+        for (iter = ages.begin(); iter != ages.end(); ++iter) {
+            for (i = 0; i < iter->second; i++)
+                printf("%d ", iter->first);
+        }
+
+        printf("\n");
+
+        ages.clear();
+    }
+
+    return 0;
+};
---
+++ problems/Parentheses Balance.cpp
@@ -0,0 +1,84 @@
+#include <cstdio>
+#include <iostream>
+#include <stack>
+
+/*
+Problem:
+
+Link: http://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=614
+
+Parentheses Balance
+You are given a string consisting of parentheses () and []. A string of this type is said to be correct:
+
+(a)
+if it is the empty string
+(b)
+if A and B are correct, AB is correct,
+(c)
+if A is correct, (A) and [A] is correct.
+Write a program that takes a sequence of strings of this type and check their correctness. Your program can assume that the maximum string length is 128.
+
+Input
+
+The file contains a positive integer n and a sequence of n strings of parentheses () and [], one string a line.
+Output
+
+A sequence of Yes or No on the output file.
+Sample Input
+
+3
+([])
+(([()])))
+([()[]()])()
+Sample Output
+
+Yes
+No
+Yes
+
+*/
+
+using namespace std;
+
+int count;
+
+string line;
+
+int main() {
+
+    freopen("input.txt", "r", stdin);
+
+    scanf("%d", &count);
+
+    while (count--) {
+
+        stack< char > pilha;
+        cin >> line;
+
+        for (int i = 0; i < line.size(); i++) {
+
+            if (line[i] == '(' || line[i] == '[') {
+                pilha.push(line[i]);
+            } else if (pilha.empty()) {
+                pilha.push(line[i]);
+                break;
+            } else if (line[i] == ')') {
+                if (pilha.top() != '(')
+                    break;
+                pilha.pop();
+            } else if (line[i] == ']') {
+                if (pilha.top() != '[')
+                    break;
+                pilha.pop();
+            }
+        }
+
+        if (!pilha.size()) {
+            printf("Yes\n");
+        } else {
+            printf("No\n");
+        }
+    }
+
+    return 0;
+};
