1c5c31aa223f32212f8470ad24d1574fc72221cc
Matheus Sampaio <matheus1401@gmail.com>
2014-07-06 17:29:06 -0300

    Reformating

--- problems/562 - Dividing coins.cpp
+++ problems/562 - Dividing coins.cpp
@@ -14,22 +14,26 @@ Link: http://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&
 int M, K, price[110]; // price[ M (<= 200)]
 int memo[110][25]; // dp table memo[money_left (<= 200)][garment_id (<= 20)]
 
-int divide(int coins_left, int diff) {
+int divide(int coins_left, int diff)
+{
 
     return 0;
 }
 
-int main() {
+int main()
+{
     int i, j, TC, score;
 
     freopen("input.txt", "r", stdin);
 
     scanf("%d", &TC);
 
-    while (TC--) {
+    while (TC--)
+    {
         scanf("%d", &M);
 
-        for (i = 0; i < M; i++) {
+        for (i = 0; i < M; i++)
+        {
             scanf("%d", &K);
 
             price[i] = K; // to simplify coding, we store K in price[i][0]
