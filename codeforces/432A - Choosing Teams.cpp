992bc87ec3db301ea9bef281b2b04c3bcaaf1731
Matheus Sampaio <matheussampaio@users.noreply.github.com>
2014-07-25 15:59:37 -0300

    Passed

---
+++ codeforces/432A - Choosing Teams.cpp
@@ -0,0 +1,29 @@
+/*
+
+  Link: http://codeforces.com/problemset/problem/432/A
+
+*/
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int n, k, persons, valids;
+
+
+int main() {
+	
+	scanf("%d %d", &n, &k);
+	
+	for (int i=0; i < n; i++) {
+		scanf("%d", &persons);
+		
+		if (persons + k <= 5) {
+			valids++;
+		}
+	}
+		
+	printf("%d", valids / 3);
+	
+	return 0;
+}
