6e997d5b17deb13478dd126348296bbdf8988a5c
Matheus Sampaio <matheus1401@gmail.com>
2014-07-28 22:27:36 -0300

    Template

---
+++ UVa/template.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+
+using namespace std;
+
+/*
+
+Link:
+
+*/
+
+int main() {
+
+    // freopen("input.txt", "r", stdin);
+
+
+    return 0;
+}
