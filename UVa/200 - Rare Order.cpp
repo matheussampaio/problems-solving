c45edef60cdacdbdfd61eb084c23db15c5621de7
Matheus Sampaio <matheus1401@gmail.com>
2014-07-29 22:39:50 -0300

    UVa 200 - Init

---
+++ UVa/200 - Rare Order.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+
+using namespace std;
+
+/*
+
+Link:
+
+*/
+
+int main() {
+
+    // freopen("input.txt", "r", stdin);
+
+    cout << "Hi!";
+
+    return 0;
+}
