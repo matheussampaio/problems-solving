3a1feb1b7488910d3589324fa366c58fa706e075
Matheus Sampaio <matheus1401@gmail.com>
2014-08-03 10:49:19 -0300

    Template updated

---
+++ template.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <sstream>
+#include <cmath>
+
+// Shortcuts for "common" data types in contests 
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef set<int> si;
+typedef map<string, int> msi;
+
+// To simplify repetitions/loops, Note: define your loop style and stick with it! 
+#define REP(i, a, b) \ for (int i = int(a); i <= int(b); i++) // a to b, and variable i is local! 
+#define TRvi(c, it) \ for (vi::iterator it = (c).begin(); it != (c).end(); it++) 
+#define TRvii(c, it) \ for (vii::iterator it = (c).begin(); it != (c).end(); it++) 
+#define TRmsi(c, it) \ for (msi::iterator it = (c).begin(); it != (c).end(); it++)
+
+using namespace std;
+
+/*
+
+Link:
+
+*/
+
+int main() {
+
+    // freopen("input.txt", "r", stdin);
+
+
+    return 0;
+}
