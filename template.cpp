26a153eae072109740a5c3341de95e97c171c238
Matheus Sampaio <matheussampaio@users.noreply.github.com>
2014-08-03 22:01:55 -0300

    Removido algumas coisas.
    Removido algumas coisas que não sei para que servem. :/

--- template.cpp
+++ template.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
 #include <cstdio>
+#include <cstdlib>
 #include <cstring>
-#include <stack>
-#include <queue>
-#include <vector>
-#include <sstream>
+#include <algorithm>
 #include <cmath>
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <ctime>
+#include <sstream>
 
-// Shortcuts for "common" data types in contests 
+using namespace std;
+
+// Shortcuts for "common" data types in contests
 typedef long long ll;
-typedef vector<int> vi;
-typedef pair<int, int> ii;
-typedef vector<ii> vii;
-typedef set<int> si;
-typedef map<string, int> msi;
+#define inf 0x3f3f3f3f
+#define Inf 0x3FFFFFFFFFFFFFFFLL
 
-// To simplify repetitions/loops, Note: define your loop style and stick with it! 
-#define REP(i, a, b) \ for (int i = int(a); i <= int(b); i++) // a to b, and variable i is local! 
-#define TRvi(c, it) \ for (vi::iterator it = (c).begin(); it != (c).end(); it++) 
-#define TRvii(c, it) \ for (vii::iterator it = (c).begin(); it != (c).end(); it++) 
-#define TRmsi(c, it) \ for (msi::iterator it = (c).begin(); it != (c).end(); it++)
+// To simplify repetitions/loops, Note: define your loop style and stick with it!
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define Rep(i, n) for (int i = 1; i <= (n); ++i)
+#define clr(x, a) memset(x, (a), sizeof x)
 
 using namespace std;
 
