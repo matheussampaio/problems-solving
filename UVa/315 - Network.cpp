a51a91fa7e1849d85fb62c8188a8d6f305dafdcf
Matheus Sampaio <matheussampaio@users.noreply.github.com>
2014-08-03 22:00:44 -0300

    Accepted!! :D

--- UVa/315 - Network.cpp
+++ UVa/315 - Network.cpp
@@ -1,69 +1,95 @@
-// Shortcuts for "common" data types in contests 
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <ctime>
+#include <sstream>
+
+using namespace std;
+
+// Shortcuts for "common" data types in contests
 typedef long long ll;
-typedef vector<int> vi;
-typedef pair<int, int> ii;
-typedef vector<ii> vii;
-typedef set<int> si;
-typedef map<string, int> msi;
+#define inf 0x3f3f3f3f
+#define Inf 0x3FFFFFFFFFFFFFFFLL
 
-// To simplify repetitions/loops, Note: define your loop style and stick with it! 
+// To simplify repetitions/loops, Note: define your loop style and stick with it!
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define Rep(i, n) for (int i = 1; i <= (n); ++i)
+#define clr(x, a) memset(x, (a), sizeof x)
 
-#define REP(i, a, b) \ for (int i = int(a); i <= int(b); i++) // a to b, and variable i is local! 
-#define TRvi(c, it) \ for (vi::iterator it = (c).begin(); it != (c).end(); it++) 
-#define TRvii(c, it) \ for (vii::iterator it = (c).begin(); it != (c).end(); it++) 
-#define TRmsi(c, it) \ for (msi::iterator it = (c).begin(); it != (c).end(); it++)
-
-
-int N, dfsNumberCounter, DFS_WHITE = 0, dfs_num[110], dfs_low[110], dfsRoot, rootChildren;
+int adjMatrix[110][110];
+int n, v, a, dfsLow[110], dfsNumberCounter = 0, dfsNum[110], dfsParent[110], dfsRoot, rootChildren, articulationVertex[110];
+string line;
 
 void articulationPointAndBridge(int u) {
-	dfs_low[u] = dfs_num[u] = dfsNumberCounter++;
+	dfsLow[u] = dfsNum[u] = dfsNumberCounter++;
+	
+	Rep(v, n) {
+		if (adjMatrix[u][v] && dfsNum[v] == -1) {
+			dfsParent[v] = u;
 
-	TRvii(graph[u], v) {
-		
+			if (u == dfsRoot)
+				rootChildren++;
+	
+			articulationPointAndBridge(v);
+			
+			if (dfsLow[v] >= dfsNum[u]) {
+				articulationVertex[u] = 1;
+			}
+
+			dfsLow[u] = min(dfsLow[u], dfsLow[v]);
+		} else if (adjMatrix[u][v] && v != dfsParent[u]) {
+			dfsLow[u] = min(dfsLow[u], dfsNum[v]);
+		}
 	}
 }
 
 int main() {
-	
-	while (scanf("%d", &n) && n) {
-		
-		memset(graph, 0, sizeof graph);
-		
-		while (scanf("%d", &v) && v) {
-			
-			getline(cin, line);
-			
-			istringstream iss( line );
-		
-			while( iss >> a ) {
-				graph[v][a] = 1;
-				graph[a][v] = 1;
-			}
-		}
 
-		dfsNumberCounter = 0;
-		memset(dfs_num, DFS_WHITE, sizeof dfs_num);
+    while (scanf("%d", &n) && n) {
 
-		for (int i=0; i <= n - 1; i++) {
-			if (dfs_num[i] == DFS_WHITE) {
-				dfsRoot = i; rootChildren = 0;
-				articulationPointAndBridge(i);
-				articulation_vertex[dfsRoot] = (rootChildren > 1);
+        clr(adjMatrix, 0);
+        clr(dfsNum, -1);
+        clr(dfsLow, 0);
+		clr(articulationVertex, 0);
+		clr(dfsParent, 0);
+		
+        while (scanf("%d", &v) && v) {
+
+            getline(cin, line);
+
+            istringstream iss( line );
+
+            while ( iss >> a ) {
+                adjMatrix[v][a] = 1;
+                adjMatrix[a][v] = 1;
+            }
+        }
+		
+		Rep(i, n) {
+			if (dfsNum[i] == -1) {
+				dfsRoot = i;
+				rootChildren = 0;
+				articulationPointAndBridge(dfsRoot);
+				articulationVertex[dfsRoot] = (rootChildren > 1);
 			}
 		}
 
 		int count = 0;
 
-		for (int i=0; i <= n - 1; i++) {
-			if (articulation_vertex[i]) {
-				count++;
-			}
+		Rep(i, n) {
+			count += articulationVertex[i];
 		}
 
 		cout << count << endl;
-		
-	}
+    }
 
-}	
+}
 
