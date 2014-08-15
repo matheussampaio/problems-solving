6c70dd0c5b1610e2c1ce8cfa6b4662369862e2a0
Matheus Sampaio <matheus1401@gmail.com>
2014-08-15 13:52:10 -0300

    Another try

--- codeforces/459A - Pashmak and Garden.cpp
+++ codeforces/459A - Pashmak and Garden.cpp
@@ -28,50 +28,34 @@ typedef long long ll;
 
 using namespace std;
 
-int v[20];
 
-double dist(int xa, int ya, int xb, int yb) {
-    return sqrt((xb - xa) * (xb - xa) + (yb - ya) * (yb - ya));
-}
+
+// double dist(int xa, int ya, int xb, int yb) {
+//     return sqrt((xb - xa) * (xb - xa) + (yb - ya) * (yb - ya));
+// }
 
 int main() {
 
     // freopen("input.txt", "r", stdin);
+    int x1, y1, x2, y2, x3, y3, x4, y4;
 
-    cin >> v[0];
-    cin >> v[1];
-    cin >> v[2];
-    cin >> v[3];
+    cin >> x1 >> y1 >> x2 >> y2;
 
-    int xa = v[0], ya = v[1], xb = v[2], yb = v[3];
-
-    if ((xa == xb) || (ya == yb)) {
-
-        xa = min(v[0], v[2]);
-        ya = min(v[1], v[3]);
-        xb = max(v[0], v[2]);
-        yb = max(v[1], v[3]);
-
-        int d = dist(xa, ya, xb, yb);
-
-        if (xa + d != xb) {
-            printf("%d %d", xa + d, ya);
-        } else if (ya + d != yb) {
-            printf("%d %d", xa, ya + d);
-        }
-
-        printf(" %d %d\n", xa + d, ya + d);
-
-    } else {
-
-        int xc = xb;
-        int yc = ya;
-
-        if (dist(xc, yc, xa, ya) != dist(xc, yc, xb, yb)) {
-            cout << "-1" << endl;
+    if (x1 != x2 && y1 != y2) {
+        if (abs(x1 - x2) != abs(y1 - y2)) {
+            printf("-1\n");
         } else {
-            printf("%d %d %d %d\n", xa, yb, xc, yc);
+            printf("%d %d %d %d\n", x1, y2, x2, y1);
         }
+    } else if (y1 == y2) {
+        int dist = x1 - x2;
+
+        printf("%d %d %d %d\n", x1, y1 + dist, x2, y2 + dist);
+    } else if (x1 == x2) {
+        int dist = abs(y1 - y2);
+        printf("%d %d %d %d\n", x1 + dist, y1, x2 + dist, y2);
+    } else {
+        printf("-1\n");
     }
 
     return 0;
