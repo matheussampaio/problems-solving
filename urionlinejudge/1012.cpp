abdd487e82cf8ffdf74c7e1304d20e392b834589
Matheus Sampaio <matheus1401@gmail.com>
2014-12-23 14:13:33 -0300

    URIOnlineJudge init :D

---
+++ urionlinejudge/1012.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <ctime>
+#include <sstream>
+#include <iterator>
+
+using namespace std;
+
+// Shortcuts for "common" data types in contests
+typedef long long ll;
+#define inf 0x3f3f3f3f
+#define Inf 0x3FFFFFFFFFFFFFFFLL
+
+// To simplify repetitions/loops, Note: define your loop style and stick with it!
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define Rep(i, n) for (int i = 1; i <= (n); ++i)
+#define REP(i, j, n) for (int i = (j); i<= (n); ++i)
+#define clr(x, a) memset(x, (a), sizeof x)
+
+using namespace std;
+
+double A, B, C, pi = 3.14159;
+
+int main() {
+    cin >> A;
+    cin >> B;
+    cin >> C;
+
+    printf("TRIANGULO: %.3f\n", A * C / 2);
+    printf("CIRCULO: %.3f\n", pi * pow(C, 2));
+    printf("TRAPEZIO: %.3f\n", (A + B) * C / 2);
+    printf("QUADRADO: %.3f\n", pow(B, 2));
+    printf("RETANGULO: %.3f\n", A * B);
+
+    return 0;
+}
