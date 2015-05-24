fe07caf621199fe4231b9e22cc7585e7b6f97521
Matheus Sampaio <matheus1401@gmail.com>
2015-05-23 23:24:58 -0300

    Add 1143

---
+++ urionlinejudge/1143.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <ctime>
+#include <sstream>
+#include <iterator>
+
+using namespace std;
+
+// Shortcuts for "common" data types in contests
+typedef long long ll;
+#define inf 0x3f3f3f3f
+#define Inf 0x3FFFFFFFFFFFFFFFLL
+
+// To simplify repetitions/loops, Note: define your loop style and stick with it!
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define Rep(i, n) for (int i = 1; i <= (n); ++i)
+#define REP(i, j, n) for (int i = (j); i<= (n); ++i)
+#define clr(x, a) memset(x, (a), sizeof x)
+
+int N, i = 0;
+
+int main() {
+    cin >> N;
+
+    while (i++ < N) {
+        printf("%d %d %d\n", i, i*i, i*i*i);
+    }
+
+    return 0;
+}
