bff7ac2491dc10d30574e663f0201bd6be9a7ea1
Matheus Sampaio <matheus1401@gmail.com>
2015-09-30 22:12:10 -0500

    Add more answers and editorconfig

---
+++ urionlinejudge/1145.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <ctime>
+#include <sstream>
+#include <iterator>
+
+using namespace std;
+
+// Shortcuts for "common" data types in contests
+typedef long long ll;
+#define inf 0x3f3f3f3f
+#define Inf 0x3FFFFFFFFFFFFFFFLL
+
+// To simplify repetitions/loops, Note: define your loop style and stick with it!
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define Rep(i, n) for (int i = 1; i <= (n); ++i)
+#define REP(i, j, n) for (int i = (j); i<= (n); ++i)
+#define clr(x, a) memset(x, (a), sizeof x)
+
+int X, Y;
+
+int main() {
+    cin >> X;
+    cin >> Y;
+
+    for (int i = 1; i <= Y; i++) {
+        cout << i;
+
+        if (i % X == 0)
+            printf("\n");
+        else if (i < Y)
+            cout << " ";
+    }
+
+    return 0;
+}
